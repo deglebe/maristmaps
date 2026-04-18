1ccad9b3eca9205ac3d578a7c6f37385ed944157
Rohan <rohan.v.plante@gmail.com>
2026-04-18 03:25:41 -0400

    created locations model

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from app.extensions import db
-
+from geoalchemy2 import Geometry
 
 class User(db.Model):
     __tablename__ = "users"
@@ -7,3 +7,17 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(255), unique=True, nullable=False, index=True)
     password_hash = db.Column(db.String(255), nullable=False)
+
+class Location(db.Model):
+    __tablename__ = "locations"
+
+    id = db.Column(db.Integer, primary_key=True)
+    kind = db.Column(db.String(50), nullable=False)
+    subtype = db.Column(db.String(50), nullable=True)
+    orientation = db.Column(db.String(10), nullable=True)
+    entrance = db.Column(db.String(100), nullable=True)
+    building = db.Column(db.String(100), nullable=False, index=True)
+    floor = db.Column(db.String(20), nullable=False)
+    room = db.Column(db.String(100), nullable=True)
+    notes = db.Column(db.Text, nullable=True)
+    loc = db.Column(Geometry(geometry_type="POINT", srid=4326), nullable=False)
